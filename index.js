6fcdbd1f549fb44f9e9e13565289c3a0f1343c80
sheriff-o <sheriffafolabi95@gmail.com>
2024-07-03 16:16:59 +0100

    change server.js to index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 import express from 'express';
+import dotenv from 'dotenv';
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.get('/api/hello', (req, res) => {
@@ -23,6 +24,6 @@ app.get('/api/hello', (req, res) => {
     }
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
